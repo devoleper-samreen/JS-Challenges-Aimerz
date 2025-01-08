20d78367e8af6c427bc588b20501ec689d37d5b1
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-08 10:32:42 +0530

    Add function to convert a string to a valid number

---
+++ number/question1.js
@@ -0,0 +1,9 @@
+//Question 1: Convert a string to a valid number
+
+
+function convertToNumber(str) {
+    return Number(str);
+}
+
+console.log(convertToNumber("123")); // 123
+console.log(convertToNumber("abc")); // NaN
