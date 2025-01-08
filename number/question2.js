a57783b9415c41c58f299d20fb1db6dfe80a0547
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-08 10:39:15 +0530

    Add function to round a number to the nearest integer

---
+++ number/question2.js
@@ -0,0 +1,10 @@
+// Question 2: Round a number to the nearest integer
+
+
+function roundNumber(num) {
+    return Math.round(num);
+
+}
+
+console.log(roundNumber(4.3)); // 4
+console.log(roundNumber(4.8)); // 5
