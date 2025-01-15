b4bbde517f2ef19d70c25b45d375358b3b580f29
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-15 18:03:26 +0530

    Add function to calculate simple interest

---
+++ operator/q5.js
@@ -0,0 +1,16 @@
+//Question 5: Calculate Simple Interest
+
+
+function calculateSimpleInterest(principal, rate, time) {
+
+    // SI= P×R×T/100
+
+    return (principal * rate * time) / 100
+
+
+
+}
+
+// Example usage:
+console.log(calculateSimpleInterest(1000, 5, 2)); // Output: 100
+console.log(calculateSimpleInterest(500, 3, 4));  // Output: 60
