16769929d99161aabcda058c867aa0f7dfe0373a
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-15 17:53:36 +0530

    Add function to convert Celsius to Fahrenheit

---
+++ operator/q2.js
@@ -0,0 +1,15 @@
+//Question 2: Convert Fahrenheit to Celsius
+
+
+function convertToFahrenheit(celsius) {
+
+    // F=(°C×9/5)+32
+
+    return (celsius * 9 / 5) + 32;
+
+}
+
+// Example usage:
+console.log(convertToFahrenheit(0));   // Output: 32
+console.log(convertToFahrenheit(100)); // Output: 212
+
