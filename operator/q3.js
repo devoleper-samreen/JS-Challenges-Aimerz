9de7683e1ad240386445dc595fbb78ae9aaeaf55
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-15 17:57:57 +0530

    Add function to calculate tip based on bill amount and percentage

---
+++ operator/q3.js
@@ -0,0 +1,12 @@
+//Question 3: Calculate Tip
+
+
+function calculateTip(billAmount, tipPercentage) {
+    return (tipPercentage / 100) * billAmount;
+}
+
+// Example usage:
+console.log(calculateTip(200, 15)); // Output: 30
+console.log(calculateTip(50, 10));  // Output: 5
+
+
